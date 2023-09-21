e6702bca42a23591387ed7ad297e043c5b0da6be
luismiguelviewnext <135017297+luismiguelviewnext@users.noreply.github.com>
2023-09-21 12:53:46 +0200

    conseguido listar coches

--- src/main/java/com/proyectocrud/concesionariocrud/repositorio/CocheRepositorio.java
+++ src/main/java/com/proyectocrud/concesionariocrud/repositorio/CocheRepositorio.java
@@ -1,9 +1,10 @@
 package com.proyectocrud.concesionariocrud.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.proyectocrud.concesionariocrud.entidad.Coche;
-
+@Repository
 public interface CocheRepositorio extends JpaRepository<Coche, Long> {
     
 }
